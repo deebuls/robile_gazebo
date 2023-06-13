6b6ed2ffb66214e84e9e054ff0c7a307d40a070a
Deebul Nair <deebul.nair@h-brs.de>
2023-06-13 08:24:59 +0000

    ros2 single robot launch

--- launch/gazebo_4_wheel.launch.py
+++ launch/gazebo_4_wheel.launch.py
@@ -5,7 +5,7 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription, LaunchContext
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable, ExecuteProcess
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import Command, FindExecutable, PathJoinSubstitution, LaunchConfiguration
 from launch_ros.substitutions import FindPackageShare
@@ -19,24 +19,58 @@ def generate_launch_description():
 
 
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+    domain_id = LaunchConfiguration('domain_id')
+    namespace = LaunchConfiguration('namespace')
+    use_namespace = LaunchConfiguration('use_namespace')
+    use_rviz = LaunchConfiguration('use_rviz')
+    x_pose = LaunchConfiguration('x_pose', default='-0.0')
+    y_pose = LaunchConfiguration('y_pose', default='-0.0')
 
+    # Declare the launch arguments
+    declare_namespace_cmd = DeclareLaunchArgument(
+        'namespace',
+        default_value='',
+        description='Top-level namespace')
+
+    declare_use_namespace_cmd = DeclareLaunchArgument(
+        'use_namespace',
+        default_value='false',
+        description='Whether to apply a namespace to the navigation stack')
+    
     declare_use_sim_time_argument = DeclareLaunchArgument(
         'use_sim_time',
         default_value='true',
         description='Use simulation/Gazebo clock')
 
-    urdf_file_name = '4_wheel_config.urdf.xacro'
+    declare_use_rviz_cmd = DeclareLaunchArgument(
+        'use_rviz',
+        default_value='True',
+        description='Whether to start RVIZ')
 
-    print('urdf_file_name : {}'.format(urdf_file_name))
+    declare_domain_id_argument = DeclareLaunchArgument(
+		'domain_id',
+        default_value='',
+        description='Domain ID in which the node has to be started'
+	)
+
+    declare_robot_pose_x_argument = DeclareLaunchArgument(
+		'x_pose',
+        default_value='0.0',
+        description='robot pose x '
+	)
+    declare_robot_pose_y_argument = DeclareLaunchArgument(
+		'y_pose',
+        default_value='0.0',
+        description='robot pose y'
+	)
 
 
-    x_pose = LaunchConfiguration('x_pose', default='-2.0')
-    y_pose = LaunchConfiguration('y_pose', default='-0.5')
+
 
     world = os.path.join(
-        get_package_share_directory('turtlebot3_gazebo'),
+        get_package_share_directory('robile_gazebo'),
         'worlds',
-        'turtlebot3_house.world'
+        'cart_basement.world'
     )
 
     gzserver_cmd = IncludeLaunchDescription(
@@ -81,32 +115,38 @@ def generate_launch_description():
             }],
     )
 
+
+
     spawn_robot_gazebo_cmd = Node(package=    'gazebo_ros',
                                   executable= 'spawn_entity.py',
                                   arguments=  ['-topic', 'robot_description',
                                                '-entity', 'robile',
+                                               '-x', x_pose,
+                                               '-y', y_pose,
+                                               '-Y', '0.78'
                                               ],
                                   output=     'screen')
 
     rviz_cmd = Node(package='rviz2',
                     namespace='',
                     executable='rviz2',
-                    name='rviz2'
+                    name='rviz2',
+                    parameters=[{'use_sim_time': use_sim_time}],
     )
 
-    static_transform_cmd = Node(package="tf2_ros",
-            executable="static_transform_publisher",
-            output="screen",
-            arguments=["0","0","0","0","0","0","base_footprint","base_link"]
+    zenoh_dds_brigde_process = ExecuteProcess(
+        cmd=['zenoh-bridge-dds', '-d', domain_id] #make robile name configurable
     )
-
+    
+    print ("Use rviz ", use_rviz)
     nodes = [
+        #SetEnvironmentVariable('ROS_DOMAIN_ID', domain_id),
         rviz_cmd,
         gzserver_cmd,
         gzclient_cmd,
         robot_state_pub_cmd,
         spawn_robot_gazebo_cmd,
-        static_transform_cmd
+        #zenoh_dds_brigde_process,
     ]
 
     return LaunchDescription(nodes)
